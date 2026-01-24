d9a9f1a580081cfe375fe42469fecb2b94f6c12f
abdulwalidev <abdulwalidev@gmail.com>
2026-01-24 21:24:16 +0500

    Create set.cpp
    Set (unique, sorted)

---
+++ set.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<int> s;
+    s.insert(3);
+    s.insert(1);
+    s.insert(3);   // ignored
+
+    for(int x : s) cout << x << " ";
+}
