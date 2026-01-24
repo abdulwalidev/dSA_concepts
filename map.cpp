5b47358db9c19bfb8e98e84221c5437443086f87
abdulwalidev <abdulwalidev@gmail.com>
2026-01-24 21:23:11 +0500

    Create map.cpp
    maps code, basic

---
+++ map.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<string, int> m;
+    m["apple"] = 3;
+    m["banana"] = 5;
+
+    for(auto p : m)
+        cout << p.first << " " << p.second << endl;
+}
